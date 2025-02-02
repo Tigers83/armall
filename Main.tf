9c90430363846ddf5ebe46c64af9b78ece0f89db
Tigers83 <Reddvisionart@gmail.com>
2025-02-02 09:54:14 -0500

    happy

---
+++ Main.tf
@@ -0,0 +1,31 @@
+module "arm-calis" {
+    source = "./arm-calis/class5"
+}
+
+module "hknew" {
+    source = "./hknew/class5"
+}
+
+module "arm-japan" {
+    source = "./arm-japan/class5"
+}
+
+module "arm-ld" {
+    source = "./arm-ld/class5"
+}
+
+module "arm-nvm" {
+    source = "./arm-nvm/class5"
+}
+
+module "arm-sp" {
+    source = "./arm-sp/class5"
+}
+
+module "sydney" {
+    source = "./sydney/class5"
+}
+
+# module "github" {
+#     source = "./.github/workflows"
+# }
